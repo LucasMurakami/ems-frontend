c9b40705e8464d28c844af1be91401363413b062
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-07-28 22:31:59 -0300

    ems-frontend

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,88 @@
+import React, {useEffect, useState} from 'react'
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
+import { useNavigate } from 'react-router-dom'
+import { getDepartmentByID } from '../services/DepartmentService'
+
+const ListEmployeeComponent = () => {
+
+    const [employees, setEmployees] = useState([])
+    const [departmentName, setDepartmentName] = useState('')
+
+    const navigator = useNavigate();
+
+    useEffect(() => {
+        getAllEmployes();
+    }, [])
+
+    function getAllEmployes() {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    function addNewEmployee() {
+        navigator('/add-employee')
+    }
+
+    function updateEmployee(id) {
+        navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id) {
+        console.log(id);
+
+        deleteEmployee(id).then( (response) => {
+            console.log(response.data);
+            getAllEmployes();
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    function triggerCounter() {
+        console.count();
+    }
+
+    return (
+    <div className='container'>
+
+        <h2 className='text-center'>List of Employees</h2>
+        <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
+
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Employee ID</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email</th>
+                    <th>Employee Department</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    employees.map(employee => 
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>                        
+                            <td>{employee.email}</td>
+                            <td>{employee.departmentName}</td>
+                            <td>
+                                <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                                <button className='btn btn-danger' onClick={ () => removeEmployee(employee.id)} style={{marginLeft: 10}}>Delete</button>
+                            </td>            
+                        </tr>)
+                }
+            </tbody>
+        </table>
+
+    </div>
+  )
+
+}
+
+export default ListEmployeeComponent
---
+++ src/services/DepartmentService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const DEPARTMENT_REST_API_BASE_URL = 'https://ems-project-test-47533d242b45.herokuapp.com/api/departments';
+
+export const getAllDepartments = () => {
+    return axios.get(DEPARTMENT_REST_API_BASE_URL);
+}
+
+export const createDepartment = (department) => {
+    return axios.post(DEPARTMENT_REST_API_BASE_URL, department);
+}
+
+export const getDepartmentByID = (departmentID) => {
+    return axios.get(DEPARTMENT_REST_API_BASE_URL + '/' + departmentID);
+}
+
+export const updateDepartment = (departmentID, department) => {
+    return axios.put(DEPARTMENT_REST_API_BASE_URL + '/' + departmentID, department)
+}
+
+export const deleteDepartment = (departmentID) => {
+    return axios.delete(DEPARTMENT_REST_API_BASE_URL + '/' + departmentID);
+}
+
+axios({
+    method: 'get',
+    url: DEPARTMENT_REST_API_BASE_URL
+})
